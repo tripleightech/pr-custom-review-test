1263473961956ea71e523d8e7db2317c1f68bb8d
Sergejs Kostjucenko <sergejs@parity.io>
2021-12-16 03:12:10 +0200

    change below head

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -6,7 +6,7 @@ substrate-matrix-faucet/src/bot/index.ts:    logger.warn(`🏴‍☠️ Ignored 
 CHANGE LINE BdsfasfELOW substrate/client/tracing/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  📋 Chain specification: Local Testnet
 substrate/client/tracing/proc-maasascro/src/lib.rs:/// 2020-10-16 08:03:14  🏷 Node name: nice-glove-1401
 substrate/client/traciadfdsafng/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  👤 Role: LIGHTfdda
-+substrate/client/cli/src/runner.rs:	/// 2020-06-03 16:14:21 🏷 Node name: jolly-rod-7462
++substrate/client/cli/src/runner.rs:	/// 2020-06-03 16:14:21 🏷 Node name: jolly-rod-7462FDF
 +substrate/client/cli/src/runner.rs:	/// 2020-06-03 16:14:21 👤 RolFDFDe: FULLgfgf
 +substrate/client/cli/src/runner.rs:	/// 2020-06-03 16:14:21 💾 Database: RocksDb at /tmp/c/chains/flamingfir7/db
 CHANGE LINE ABOVE nfo!("📋 Chain specificatsfdfdsion: {}", config.chain_spec.name());
