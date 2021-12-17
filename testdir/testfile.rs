5470c8348e5ebfe4fb1ed79454fc7098585ee499
Sergejs Kostjucenko <sergejs@parity.io>
2021-12-17 15:00:25 +0200

    Touch bag

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -1,4 +1,4 @@
-👜seFGFGFradfsdfdfdddffd,12342134👜",👜",👜", substrate/frame/bags-list/src/lib.rs:
+👜seFGFGFradfsdfdfdddffd,12342134👜",👜",👜", substrate/frame/bags-list/src/lib.rs:dsfdfs
 concat!("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
 👜"some mofdsafdsre substrate/client/finality-grandpa/src/environment.rs:					"👴 Applying GRANDPA set change to new set with {} authorities",
 substrateasdsaddssddasfdsaf/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
