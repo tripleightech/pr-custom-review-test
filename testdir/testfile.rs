7920eed3f34e5af6338ce1c0b51fecd53313054c
Sergejs Kostjucenko <sergejs@parity.io>
2021-12-17 16:55:52 +0200

    Touch bag

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -1,7 +1,7 @@
 👜seFGFGFradfsdfdfdddffd,12342134👜",👜",👜", substrate/frame/bags-list/src/lib.rs:dsfdfsbbbbb
 concat!sss("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
 👜"some mofdsafdsre substrate/client/finality-grandpa/src/environment.rs:					"👴 Applying GRANDPA set change to new set with {} authorities",
-substrateasdsaddssddasfdsaf/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
+substrassssteasdsaddssddasfdsaf/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
 substrate-matrix-faucet/src/bot/index.ts:    logger.warn(`🏴‍☠️ Ignored request from an ignored account: ${sender}`);fsd
 2020-10-16 08:03:14  ✌️  version 2.0.0-47f7d3f2e-x86_64-linux-gnu🔒gnu🔒dsfasf
 CHANGE LINE BdsfasfELOW substrate/client/tracing/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  📋 Chain specification: Local Testnet
