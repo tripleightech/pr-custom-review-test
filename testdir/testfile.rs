987446fa5ae1a0f46517f7888fdd3daf98361bcf
alvicsam <alvicsam@gmail.com>
2021-12-20 18:03:42 +0100

    remove more 🔒

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -3,8 +3,7 @@ concat!("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $
 👜"some mofdsafdsre substrate/client/finality-grandpa/src/environment.rs:					"👴 Applying GRANDPA set change to new set with {} authorities",
 substrateasdsaddssddasfdsaf/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
 maybe here as well? substrate-matrix-faucet/src/bot/index.ts:    logger.warn(`🏴‍☠️ Ignored request from an ignored account: ${sender}`);fsd
-2020-10-16 08:03:14  ✌️  version 2.0.0-47f7d3f2e-x86_64-linux-gnu🔒gnu🔒dsfasf
-CHANGE LINE BdsfasfELOW substrate/client/tracing/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  📋 Chain specification: Local Testnet
+CHANGE LINE BELOW substrate/client/tracing/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  📋 Chain specification: Local Testnet
 substrate/client/tracing/proc-maasascro/src/lib.rs:/// 2020-10-16 08:03:14  🏷 Node name: nice-glove-1401
 substrate/client/traciadfdsafng/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  👤 Role: LIGHTfdda
 +substrate/client/cli/src/runner.rs:	/// 2020-06-03 16:14:21 🏷 Node name: jolly-rod-7462FDF
