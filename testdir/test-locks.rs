0647eae4684b29167334a5f6f1b3f0af2a4a8535
Sergejs Kostjucenko <sergejs@parity.io>
2021-12-20 17:45:15 +0200

    Add test data

---
+++ testdir/test-locks.rs
@@ -0,0 +1,73 @@
+/// locks  heads  bags 🔒
+/// Runtime version.
+#[sp_version::runtime_version]
+pub const VERSION: RuntimeVersion = RuntimeVersion {
+	spec_name: create_runtime_str!("node"),
+	impl_name: create_runtime_str!("substrate-node"),
+	authoring_version: 10, //
+	// Per convention: if the runtime behavior changes, increment spec_version
+	// and set impl_version to 0. If only runtime
+	// implementation changes and behavior does not, then leave spec_version as
+	// is and increment impl_version.
+	spec_version: 268,
+	impl_version: 0,
+	apis: RUNTIME_API_VERSIONS,
+	transaction_version: 2,
+};
+
+/// l🔒
+/// Runtime version.
+#[sp_version::runtime_version]
+pub const VERSION: RuntimeVersion = RuntimeVersion {
+	spec_name: create_runtime_str!("node"),
+	impl_name: create_runtime_str!("substrate-node"),
+	authoring_version: 10,
+	// Per convention: if the runtime behavior changes, increment spec_version
+	// and set impl_version to 0. If only runtime
+	// implementation changes and behavior does not, then leave spec_version as
+	// is and increment impl_version.
+	spec_version: 268,
+	impl_version: 0,
+	apis: RUNTIME_API_VERSIONS,
+	transaction_version: 2,
+};//
+
+/// locks  bags 🔒
+/// Runtime version.
+#[sp_version::runtime_version]
+pub const VERSION: RuntimeVersion = RuntimeVersion {
+	spec_name: create_runtime_str!("node"), bags 🔒
+	impl_name: create_runtime_str!("substrate-node"),
+	authoring_version: 10,
+     bags 🔒	// Per convention: if the runtime behavior changes, increment spec_version
+	// and set impl_version to 0. If only runtime
+	// implementation changes and behavior does not, then leave spec_version as
+	// is and increment impl_version.
+	spec_version: 268,
+	impl_version: 0, bags 🔒
+	apis: RUNTIME_API_VERSIONS,
+	transaction_version: 2,
+};// bags 🔒
+
+/// heads  bags 🔒
+/// Runtime version.
+#[sp_version::runtime_version] // bags 🔒
+pub const VERSION: RuntimeVersion = RuntimeVersion {
+	spec_name: create_runtime_str!("node"),
+	impl_name: create_runtime_str!("substrate-node"),
+	authoring_version: 10,// bags 🔒
+	// Per convention: if the runtime behavior changes, increment spec_version
+	// and set impl_version to 0. If only runtime
+	// implementation  bags 🔒changes and behavior does not, then leave spec_version as
+	 bags 🔒// is and increment impl_version.
+	spec_version: 268,
+	impl_version: 0,
+	apis: RUNTIME_API_VERSIONS,
+	transaction_version: 2,
+};
+
+ bags 🔒
+/// locks  heads  bags 🔒                                🔒
+🔒
+                                                                🔒
+🔒
