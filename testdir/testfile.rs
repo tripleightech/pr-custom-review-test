d6dbeed0145c956c1c9060ef266be78d3e020459
alvicsam <alvicsam@gmail.com>
2021-12-20 17:55:36 +0100

    one more test

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -4,10 +4,6 @@ concat!("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $
 substrateasdsaddssddasfdsaf/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
 substrate-matrix-faucet/src/bot/index.ts:    logger.warn(`🏴‍☠️ Ignored request from an ignored account: ${sender}`);fsd
 2020-10-16 08:03:14  ✌️  version 2.0.0-47f7d3f2e-x86_64-linux-gnu🔒gnu🔒dsfasf
-
-
-
-some text here?
 CHANGE LINE BdsfasfELOW substrate/client/tracing/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  📋 Chain specification: Local Testnet
 substrate/client/tracing/proc-maasascro/src/lib.rs:/// 2020-10-16 08:03:14  🏷 Node name: nice-glove-1401
 substrate/client/traciadfdsafng/proc-macro/src/lib.rs:/// 2020-10-16 08:03:14  👤 Role: LIGHTfdda
@@ -18,6 +14,7 @@ CHANGE LINE ABOVE nfo!("📋 Chain specificatsfdfdsion: {}", config.chain_spec.n
 🔒subssssstratssssdfdssaasdfasasaedsafds/client/dsdsdcli/src/runner.rs🔒	info!("🏷 Node name: {}", config.network.node_name);kcdsfsdfsd
 sss subsdasfadstrate/client/cli/src/runner.rs:	info!("👤 Role: {}", config.display_role());
 dsdssubstrate/client/cli/src/dsdsdsrunner.rs:dfadssdafd	"💾 Database: {} at {}",
+here should be fine, no?
 concat!("[#{:?}] 🗳  ", $patterasdassssdsan), <frame_system::Pallet<T>>::block_number() $(, $values)*
 fdasdfdasertertrewadsadsdasdsafasdffdsgdsfdasdf
 dsdfsfdssdfdsadfsdaffadsfadssadsdsdds
